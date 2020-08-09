897549a731641ea69a2e5d871db6f50704dc7051
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-09 11:48:11 +0700

    Create webpack.prod.js

---
+++ webpack.prod.js
@@ -0,0 +1,20 @@
+module.exports = merge(common, {
+    mode: "production",
+    module: {
+        rules: [
+            /* babel loader */
+            {
+                test: /\.js$/,
+                exclude: "/node_modules/",
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-dev"]
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+})
