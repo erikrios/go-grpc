d8aa78b917d212e7eb905aba78816651047610e6
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-09 11:26:21 +0700

    Add HTML Webpack Plugin webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
     plugins: [
         /* HTML Webpack Plugin */
         new HtmlWebpackPlugin({
-            template: "./index.html",
+            template: "./src/template.html",
             filename: "index.html"
         })
     ]
